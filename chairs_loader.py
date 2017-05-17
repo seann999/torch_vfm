f08f53cb8ba3b7de4b7eb1a7a3c1396e19b36a4d
sean <seann999@gmail.com>
2017-05-17 15:39:35 +0900

    chairs

--- chairs_loader.py
+++ chairs_loader.py
@@ -5,7 +5,7 @@ import torch.utils.data
 import os
 import numpy as np
 
-root = "/Users/sean/data/rendered_chairs"
+root = "/media/sean/HDCL-UT1/rendered_chairs"
 dirs = []
 
 for d in os.listdir(root):
@@ -15,10 +15,29 @@ for d in os.listdir(root):
 train_chairs = dirs[:1000]
 test_chairs = dirs[1000:]
 margin = 100
+steps = 10
+rot_steps = 5
 
 class ChairsDataset(torch.utils.data.Dataset):
     def __init__(self, chairs):
         self.chairs = chairs
+        self.cache = {}
+
+
+
+        print("loading dataset...")
+        for idx in range(len(self.chairs)):
+            print(idx, len(self.chairs))
+            renders = os.path.join(root, self.chairs[idx], "renders")
+            chair_imgs = os.listdir(renders)
+            for chair_name in chair_imgs:
+                path = os.path.join(renders, chair_name)
+                img = cv2.imread(path)[margin:600 - margin, margin:600 - margin,
+                      [2, 1, 0]] / 255.0
+                img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_LINEAR)
+                self.cache[path] = np.moveaxis(img.astype(np.float32), -1, 0)
+
+
 
     def __len__(self):
         return len(self.chairs)
@@ -27,41 +46,64 @@ class ChairsDataset(torch.utils.data.Dataset):
         renders = os.path.join(root, self.chairs[idx], "renders")
         chair_imgs = os.listdir(renders)
         seq = []
-        mats = []
+        np.random.seed(np.fromstring(os.urandom(24), dtype=np.uint32))
         offset = np.random.randint(len(chair_imgs) - 1)
 
-        for chair_name in chair_imgs[offset:offset+2]:
-            img = cv2.imread(os.path.join(renders, chair_name))[margin:600-margin, margin:600-margin, [2, 1, 0]] / 255.0
-            img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_LINEAR)
-
-            mats.append(np.moveaxis(img.astype(np.float32), -1, 0))
-
-        offset = 0
-
         for i in range(len(chair_imgs)):
-            def get_data(i):
-                chair_idx = (i + offset) % len(chair_imgs)
+            def get_data(i, basey=0, diff=500):
+                chair_idx = i % len(chair_imgs)
                 chair_name = chair_imgs[chair_idx]
-                mat = mats[chair_idx]
 
                 tokens = chair_name.split("_")
                 rotx = int(tokens[2][1:])
                 roty = int(tokens[3][1:])
 
+                diffy = (roty - basey + 360) % 360
+                if diffy > 180:
+                    diffy -= 360
+
+                if abs(diffy) > diff:
+                    return None
+
+                path = os.path.join(renders, chair_name)
+
+
+                if path not in self.cache:
+                    print("adding to cache: ", path)
+                    img = cv2.imread(path)[margin:600 - margin, margin:600 - margin,
+                          [2, 1, 0]] / 255.0
+                    img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_LINEAR)
+                    self.cache[path] = np.moveaxis(img.astype(np.float32), -1, 0)
+
+
+                mat = self.cache[path]
+
                 return mat, roty, rotx
 
-            mat1, roty1, rotx1 = get_data(i)
-            mat2, roty2, rotx2 = get_data(i+1)
+            mat1, roty1, rotx1 = get_data(offset)
+
+            offset = 0
+            re = None
+            while re is None:
+                offset += np.random.randint(len(chair_imgs) - 1)
+                re = get_data(offset, basey=roty1, diff=500)
+            mat2, roty2, rotx2 = re
 
             diffy = (roty2 - roty1 + 360) % 360
+
+            if diffy > 180:
+                diffy -= 360
+
             diffx = rotx2 - rotx1
 
-            seq.append((mat1, (diffy, diffx), mat2))
-            break
+            seq.append((mat1, [diffy, diffx], mat2))
+
+            if len(seq) >= steps:
+                break
 
         mat1, act, mat2 = zip(*seq)
 
-        return mat1, act, mat2
+        return torch.FloatTensor(np.array(mat1)), torch.FloatTensor(np.array(act)), torch.FloatTensor(np.array(mat2))
 
 if __name__ == "__main__":
     train_loader = torch.utils.data.DataLoader(
--- vfm.py
+++ vfm.py
@@ -20,6 +20,13 @@ args = parser.parse_args()
 configure(args.model, flush_secs=5)
 
 s=16
+img_size = 64
+rnn_size = 512
+rnn_input_size = 256
+layers = 1
+batch_size = 32
+z_size = 128
+action_size = 2
 
 class VFM(nn.Module):
 
@@ -38,15 +45,17 @@ class VFM(nn.Module):
         )
 
         self.en2 = torch.nn.Sequential(
-            nn.Linear(s * s * 128, 256),
+            nn.Linear(s * s * 128 + action_size, rnn_input_size),
             torch.nn.ELU()
         )
 
-        self.mean = nn.Linear(256, 64)
-        self.logvar = nn.Linear(256, 64)
+        self.rnn = nn.LSTM(rnn_input_size, rnn_size, layers, batch_first=True)
+
+        self.mean = nn.Linear(rnn_size, z_size)
+        self.logvar = nn.Linear(rnn_size, z_size)
 
         self.de = torch.nn.Sequential(
-            nn.Linear(64, 256),
+            nn.Linear(z_size, 256),
             torch.nn.ELU(),
             nn.Linear(256, s * s * 128),
             torch.nn.ELU()
@@ -66,39 +75,56 @@ class VFM(nn.Module):
     def reparameterize(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
         eps = torch.FloatTensor(std.size()).normal_()
-        eps = Variable(eps)
+        eps = Variable(eps).cuda()
         return eps.mul(std).add_(mu)
 
-    def encode(self, x):
+    def encode(self, x, acts):
+        x = x.view(batch_size*chairs.steps, 3, img_size, img_size)
         h = self.en(x)
-        #print(h, "en")
-        h = self.en2(h.view(-1, s*s*128))
+
+        img_enc = h.view(-1, s*s*128)
+        acts = acts.view(-1, 2)
+
+        h = self.en2(torch.cat([img_enc, acts], dim=1))
+
+        rnn_h = Variable(torch.zeros(layers, batch_size, rnn_size)).cuda()
+        rnn_c = Variable(torch.zeros(layers, batch_size, rnn_size)).cuda()
+        h = h.view(batch_size, chairs.steps, -1)
+
+        h, (_, _) = self.rnn(h, (rnn_h, rnn_c))
+
+        h = h.contiguous().view(batch_size*chairs.steps, rnn_size)
 
         mean = self.mean(h)
         logvar = self.logvar(h)
 
         z = self.reparameterize(mean, logvar)
 
+        mean = mean.view(batch_size, chairs.steps, -1)
+        logvar = logvar.view(batch_size, chairs.steps, -1)
+
         return z, mean, logvar
 
     def decode(self, z):
+        z = z.view(batch_size*chairs.steps, -1)
+        #print(z.size())
         h = self.de(z)
         out = self.de2(h.view(-1, 128, s, s))
-        #print(out)
+        out = out.view(batch_size, chairs.steps, 3, img_size, img_size)
 
         return out
 
-    def forward(self, x):
-        z, mean, logvar = self.encode(x)
+    def forward(self, x, acts):
+        z, mean, logvar = self.encode(x, acts)
         x_ = self.decode(z)
 
         return x_, mean, logvar
 
     def loss(self, x, recon, mean, logvar):
-        bce = -(x * torch.log(recon + 1e-5) + (1.0 - x) * torch.log(1.0 - recon + 1e-5)).sum(1).sum(2).sum(3).mean()
+        bce = -(x * torch.log(recon + 1e-5) + (1.0 - x) * torch.log(1.0 - recon + 1e-5)).sum(1).sum(2).sum(3).sum(4).mean()
 
         KLD_element = mean.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-        KLD = KLD_element.mul_(-0.5).sum(1).mean()
+        KLD = KLD_element.mul_(-0.5).sum(1).sum(2).mean()
 
         return bce + KLD
 
@@ -107,13 +133,25 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
 
-if __name__ == "__main__":
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+def train():
     train_loader = torch.utils.data.DataLoader(
         ChairsDataset(chairs.train_chairs),
-        batch_size=8, shuffle=True,
-        num_workers=4, pin_memory=False)
+        batch_size=batch_size, shuffle=True,
+        num_workers=8, pin_memory=False, drop_last=True)
 
-    model = VFM()
+    test_loader = torch.utils.data.DataLoader(
+        ChairsDataset(chairs.test_chairs),
+        batch_size=batch_size, shuffle=True,
+        num_workers=2, pin_memory=False, drop_last=True)
+
+    model = VFM().cuda()
     epoch = 0
 
     if os.path.isfile('{}/checkpoint.tar'.format(args.model)):
@@ -124,8 +162,8 @@ if __name__ == "__main__":
               .format(checkpoint['epoch']))
 
     # model.apply(weights_init)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    # hidden = model.init_hidden(args.batch_size)
 
     while True:
         torch.save({
@@ -133,44 +171,89 @@ if __name__ == "__main__":
             'state_dict': model.state_dict()
         }, '{}/checkpoint.tar'.format(args.model))
 
+        def run(train):
+            total_loss = 0
 
-        for k in range(20):
-            lerps = []
-            start = np.random.uniform(-2, 2, (1, 64))
-            end = np.random.uniform(-2, 2, (1, 64))
+            if train:
+                title = "train"
+            else:
+                title = "test"
 
-            for x in np.linspace(0, 1, 10):
-                gen = model.decode(Variable(torch.FloatTensor(end*x+start*(1-x))))
-                lerps.append(np.moveaxis(gen.data.numpy()[0], 0, -1)*255.0)
+            if train:
+                loader = train_loader
+            else:
+                loader = test_loader
 
-            cv2.imwrite("image-{}.png".format(k), np.hstack(lerps))
+            for i, (mat1, act, mat2) in enumerate(loader):
+                x = Variable(mat1).cuda()
+                y = Variable(mat2).cuda()
+
+                acts = Variable(act).cuda()
+
+                # hidden = repackage_hidden(hidden)
+                optimizer.zero_grad()
+                recon, mean, logvar = model.forward(x, acts)
+
+                #if train:
+                loss = model.loss(y, recon, mean, logvar)
+                total_loss += loss.data[0]
+                loss.backward()
+
+                if train:
+                    optimizer.step()
+
+                x_img = np.hstack([np.moveaxis(x.data.cpu().numpy()[0, m, ...], 0, -1) for m in range(chairs.steps)])
+                y_img = np.hstack([np.moveaxis(y.data.cpu().numpy()[0, m, ...], 0, -1) for m in range(chairs.steps)])
+                pred_img = np.hstack([np.moveaxis(recon.data.cpu().numpy()[0, m, ...], 0, -1) for m in range(chairs.steps)])
+
+                cv2.imshow(title, np.vstack([x_img, y_img, pred_img]))
+                cv2.waitKey(1)
+
+                if train:
+                    log_value("{} loss".format(title), loss.data[0], i + epoch * len(train_loader))
+
+                    print("epoch {}, step {}/{}: {}".format(epoch, i, len(train_loader), loss.data[0]))
+
+                print(act.numpy()[0])
 
 
-        total_loss = 0
+            epoch_loss = total_loss / len(loader)
+            log_value('epoch {} loss'.format(title), epoch_loss, epoch)
+            print("AVG LOSS: {}".format(epoch_loss))
 
-        for i, (mat1, act, mat2) in enumerate(train_loader):
-            x = Variable(mat1[0])
-
-            optimizer.zero_grad()
-            recon, mean, logvar = model.forward(x)
-
-            loss = model.loss(x, recon, mean, logvar)
-            total_loss += loss.data[0]
-            loss.backward()
-
-            optimizer.step()
-            #print(recon.data.numpy()[0].shape)
-            #print(np.amax(x.data.numpy()[0]))
-            cv2.imshow("orig", np.moveaxis(x.data.numpy()[0], 0, -1))
-            cv2.imshow("img", np.moveaxis(recon.data.numpy()[0], 0, -1))
-            cv2.waitKey(1)
-
-            log_value('loss', loss.data[0], i + epoch * len(train_loader))
-
-            print("epoch {}, step {}/{}: {}".format(epoch, i, len(train_loader), loss.data[0]))
-
-        epoch_loss = total_loss / len(train_loader)
-        log_value('epoch loss', epoch_loss, epoch)
-        print("AVG LOSS: {}".format(epoch_loss))
-
+        run(True)
+        run(False)
         epoch += 1
+
+def lerp():
+    global batch_size
+    batch_size = 1
+    chairs.steps = 1
+
+    model = VFM().cuda()
+
+    if os.path.isfile('{}/checkpoint.tar'.format(args.model)):
+        checkpoint = torch.load('{}/checkpoint.tar'.format(args.model))
+        model.load_state_dict(checkpoint['state_dict'])
+        print("=> loaded checkpoint (epoch {})"
+              .format(checkpoint['epoch']))
+
+
+    while True:
+        print("generating")
+        lerps = []
+        start = np.random.uniform(-2, 2, (1, z_size))
+        end = np.random.uniform(-2, 2, (1, z_size))
+
+        for x in np.linspace(0, 1, 10):
+            gen = model.decode(Variable(torch.FloatTensor(end*x+start*(1-x))).cuda())
+            lerps.append(np.moveaxis(gen.data.cpu().numpy()[0, 0, ...], 0, -1))
+
+        cv2.imshow("lerp", np.hstack(lerps))
+        cv2.waitKey(0)
+        #cv2.imwrite("image-{}.png".format(k), np.hstack(lerps))
+        print("generated ")
+
+if __name__ == "__main__":
+    #train()
+    lerp()
